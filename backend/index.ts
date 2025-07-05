76185f2f834b3fbc26a871bb3d4548ba1eedcdcc
SakaiNizinosuke <nizihallal@gmail.com>
2025-07-06 02:32:41 +0900

    implement getBands

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import path from "path";
 import { getMembersHandler } from "./getMembersHandler";
 import { uploadBandHander } from "./uploadBand";
+import { getBandsHandler } from "./getBandsHandler";
 
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
@@ -21,7 +22,7 @@ app.use(express.json())
 app.post("/upload/csv", uploadCsvHandler);
 app.get("/members", getMembersHandler);
 app.post("/upload/band", uploadBandHander);
-
+app.get("/bands", getBandsHandler)
 
 const PORT = 3001;
 app.listen(PORT, () => {
