c17b7e041de60d6aa080dd79a133d1571a44a36c
SakaiNizinosuke <nizihallal@gmail.com>
2025-07-06 00:17:47 +0900

    implement file upload to DynamoDB

--- backend/uploadCsv.ts
+++ backend/uploadCsv.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import { parse } from "csv-parse/sync";
 import AWS from "aws-sdk";
+import dotenv from "dotenv";
+import path from "path"
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const dynamo = new AWS.DynamoDB.DocumentClient({
     region: process.env.AWS_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 const TABLE_NAME = process.env.DYNAMO_TABLE_NAME!;
 
@@ -18,16 +19,38 @@ export const uploadCsvHandler = async (req: Request, res: Response) => {
             skip_empty_lines: true,
         });
 
+        console.log("パース後のレコード数:", records.length);
+        console.log("最初のレコード:", records[0]);
+
         for (const record of records) {
-            await dynamo
-                .put({
-                    TableName: TABLE_NAME,
-                    Item: record,
-                })
-                .promise();
+            try {
+                if (!record.id) {
+                    throw new Error("CSVのレコードに'id'フィールドがありません")
+                }
+                record.id = Number(record.id)
+                if (Number.isNaN(record.id)) {
+                    throw new Error(`idの値が数値に変換できませんでした: ${record.id}`)
+                }
+
+                console.log("A")
+                await dynamo
+                    .put({
+                        TableName: TABLE_NAME,
+                        Item: record,
+                    })
+                    .promise();
+
+                console.log("B")
+            } catch (err: any) {
+                console.error("DynamoDBへの書き込みに失敗しました", err)
+            }
         }
 
+        console.log("hehe")
+
         res.status(200).json({ message: "CSV imported to DynamoDB"});
+        console.log("hoge")
+
     } catch (err: any) {
         console.error(err);
         res.status(500).json({ error: err.message });
